ace1f1ab8ba1ecbbcef9965bfd5af19bee899c2a
Yasmin-Haitham <66221948+Yasmin-Haitham@users.noreply.github.com>
2021-11-27 13:58:51 +0200

    middleware update

--- main.js
+++ main.js
@@ -8,12 +8,21 @@ let userRouter = require('./routes/user');
 //middleware
 app.use('/',(req,res,next)=>{
     let today = new Date();
-    if(today.getDay() === 5 || today.getDay() === 6){
-        next();
-    }else{
+    if(today.getDay() === 0 || today.getDay() === 6){
         res.send("we are off today")
+    }else{
+        next();
     }
 })
+app.use('/',(req,res,next)=>{
+    let today = new Date();
+    if(today.getHours()>=9 && today.getHours()<=17){
+        next();
+    }else{
+        res.send("we are not available now, Working hours between 9 am to 5 pm ")
+    }
+})
+
 
 //routes
 app.use('/',userRouter);
